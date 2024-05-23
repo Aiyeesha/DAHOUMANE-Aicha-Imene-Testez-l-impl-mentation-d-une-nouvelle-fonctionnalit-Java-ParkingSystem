cef282b9cffde03c4450421223b62eb932bb926d
Aïcha Imène DAHOUMANE <139870633+Aiyeesha@users.noreply.github.com>
2024-05-24 00:18:15 +0200

    Update DataBasePrepareService.java

--- src/test/java/com/parkit/parkingsystem/integration/service/DataBasePrepareService.java
+++ src/test/java/com/parkit/parkingsystem/integration/service/DataBasePrepareService.java
@@ -1,30 +1,52 @@
-package com.parkit.parkingsystem.integration.service;
+package com.parkit.parkingsystem.integration.config;
 
-import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
+import com.parkit.parkingsystem.config.DataBaseConfig;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.sql.Connection;
+import java.sql.*;
 
-public class DataBasePrepareService {
+public class DataBaseTestConfig extends DataBaseConfig {
 
-    DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
+    private static final Logger logger = LogManager.getLogger("DataBaseTestConfig");
 
-    public void clearDataBaseEntries(){
-        Connection connection = null;
-        try{
-            connection = dataBaseTestConfig.getConnection();
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        logger.info("Create DB connection");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/test?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","rootroot");
+    }
 
-            //set parking entries to available
-            connection.prepareStatement("update parking set available = true").execute();
-
-            //clear ticket entries;
-            connection.prepareStatement("truncate table ticket").execute();
-
-        }catch(Exception e){
-            e.printStackTrace();
-        }finally {
-            dataBaseTestConfig.closeConnection(connection);
+    public void closeConnection(Connection con){
+        if(con!=null){
+            try {
+                con.close();
+                logger.info("Closing DB connection");
+            } catch (SQLException e) {
+                logger.error("Error while closing connection",e);
+            }
         }
     }
 
+    public void closePreparedStatement(PreparedStatement ps) {
+        if(ps!=null){
+            try {
+                ps.close();
+                logger.info("Closing Prepared Statement");
+            } catch (SQLException e) {
+                logger.error("Error while closing prepared statement",e);
+            }
+        }
+    }
 
+    public void closeResultSet(ResultSet rs) {
+        if(rs!=null){
+            try {
+                rs.close();
+                logger.info("Closing Result Set");
+            } catch (SQLException e) {
+                logger.error("Error while closing result set",e);
+            }
+        }
+    }
 }
